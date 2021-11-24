e99bf182ef3830b3cae489f60840803b1236a882
ppoliani <ppoliani@gmail.com>
2021-11-24 18:54:50 +0000

    feat(*): implement the private  exec_transfer event

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@ elrond_wasm::imports!();
 
 #[elrond_wasm_derive::contract]
 pub trait ERC20 {
+  // Storage
   #[view(totalSupply)]
   #[storage_mapper("total_supply")]
   fn total_supply(&self) -> SingleValueMapper<BigUint>;
@@ -16,6 +17,23 @@ pub trait ERC20 {
   #[storage_mapper("allowance")]
   fn allowance(&self, owner: &ManagedAddress, spender: &ManagedAddress) -> SingleValueMapper<BigUint>;
   
+  // Events
+  #[event("transfer")]
+  fn transfer_event(
+    &self,
+    #[indexed] sender: &ManagedAddress,
+    #[indexed] recipient: &ManagedAddress,
+    amount: &BigUint
+  );
+
+  #[event("approve")]
+  fn approve_event(
+    &self,
+    #[indexed] owner: &ManagedAddress,
+    #[indexed] spender: &ManagedAddress,
+    amount: &BigUint,
+  );
+
   #[init]
   fn init(&self, total_supply: &BigUint) {
     let creator = self.blockchain().get_caller();
@@ -25,5 +43,27 @@ pub trait ERC20 {
 
     // total supply is pre-minted and sent to the deployer
     self.balance_of(&creator).update(|balance| *balance += total_supply);
+  }
+
+  fn exec_transfer(
+    &self,
+    sender: ManagedAddress,
+    recipient: ManagedAddress,
+    amount: BigUint
+  ) -> SCResult<()> {
+    // check sender's balance
+    self.balance_of(&sender).update(|balance| {
+      require!(amount <= *balance, "insufficient funds");
+      *balance -= &amount;
+
+      Ok(())
+    })?;
+
+    // increase recipients amount
+    self.balance_of(&recipient).update(|balance| *balance += &amount);
+
+    self.transfer_event(&sender, &recipient, &amount);
+
+    Ok(())
   }
 }
